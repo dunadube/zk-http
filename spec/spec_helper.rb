5fb64bdf90ef7b6403ec76320282bdf00cdf6f6f
Stefan Huber <stefan.huber@friendscout24.de>
2011-12-28 11:32:18 +0100

    finished specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,34 +4,83 @@ require 'bundler/setup'
 require 'rspec'
 require 'rspec/autorun'
 
+module MockHelpers
 
-# method to waith until block passed returns true or timeout (default is 10 seconds) is reached 
-def wait_until(timeout=10, &block)
-  time_to_stop = Time.now + timeout
-  until yield do 
-    break unless Time.now < time_to_stop
+  def create_http_pool_which_raises_exception_on_request(svcname, exception_class, number_of_exceptions=1)
+    http_mock = double(Net::HTTP)
+    http_mock.stub(:start) {} 
+
+    raise_exception_once = raise_times(exception_class, number_of_exceptions)
+    http_mock.stub(:request) do 
+      raise_exception_once.call
+
+      # second time return a valid response
+      resp = double(Net::HTTPResponse)
+      resp.stub(:http_version) {}
+      resp.stub(:body) {"request url was #{req.path}"}
+      resp
+    end
+    if number_of_exceptions == 1
+      # http pool should try the request, fail, finish the connection
+      # and retry with another connection
+      http_mock.should_receive(:finish).once
+    else
+      # http pool will give up when the second attempt also fails
+      # and will subsequently close the second connection too
+      http_mock.should_receive(:finish).twice
+    end
+
+    strategy = double(ZK::FirstInListStrategy)
+    strategy.stub(:svcname) {svcname}
+    strategy.stub(:create_http_connection) { http_mock }
+
+    ZK::HttpClientPooled.new(:connection_strategy => strategy)
+  end
+
+  def create_http_pool_which_raises_error_on_start(svcname)
+    http_mock = double(Net::HTTP)
+    http_mock.stub(:start) { raise Errno::ECONNREFUSED.new("exception when trying to connect to service instance")}
+
+    strategy = double(ZK::FirstInListStrategy)
+    strategy.stub(:svcname) {svcname}
+    strategy.stub(:create_http_connection) { http_mock }
+
+    ZK::HttpClientPooled.new(:connection_strategy => strategy)
+  end
+
+  def create_http_pool_with_first_in_list_strategy(svcname)
+    http_mock = double(Net::HTTP)
+    http_mock.stub(:start) {}
+    http_mock.stub(:request) do |req| 
+      resp = double(Net::HTTPResponse)
+      resp.stub(:http_version) {}
+      resp.stub(:body) {"request url was #{req.path}"}
+      resp
+    end
+
+    strategy = double(ZK::FirstInListStrategy)
+    strategy.stub(:svcname) {svcname}
+    strategy.stub(:create_http_connection) { http_mock }
+
+    ZK::HttpClientPooled.new(:connection_strategy => strategy)
+  end
+
+  private
+
+  # Returns a proc which raises the specified
+  # exception the specified number of times
+  # (in sequence).
+  # Afterwards it will just do nothing when 
+  # being called.
+  def raise_times(exception_class, max=1) 
+    count = 0
+    Proc.new do 
+      if count < max
+        count = count + 1
+        raise exception_class.new 
+      end
+      count = count + 1
+    end
   end
 end
 
-# silent watcher for testing
-class SilentWatcher; def process(event); end; end
-
-class EventWatcher
-  
-  attr_reader :events, :received_disconnected
-  
-  def initialize
-    @received_disconnected = false
-    @events = []
-  end
-  
-  def process(event)
-    @events << event
-    @received_disconnected = event.state == ZooKeeper::WatcherEvent::KeeperStateDisconnected
-  end
-  
-  def event_types
-    @events.collect { |e| e.type }
-  end
-  
-end
--- spec/zk_http_spec.rb
+++ spec/zk_http_spec.rb
@@ -2,89 +2,72 @@ require File.join(File.dirname(__FILE__), %w[spec_helper])
 require 'rspec/mocks'
 require File.dirname(__FILE__) + "/../lib/zk-http.rb"
 
-class ZK::FirstInListStrategyMock < ZK::FirstInListStrategy
-  include RSpec::Mocks
-
-  def initialize(svcname)
-    @svcname = svcname
-  end
-
-  def service_instances
-    [["foo", 4567],["bar", 7890]]
-  end
-
-  def create_http_connection(failed_instance=nil)
-    service_instance = next_best_instance(failed_instance)
-    p service_instance
-
-    mock_connection = double(Net::HTTP, *service_instance)
-    mock_connection.stub(:start) {} 
-
-    mock_connection.stub(:start) do
-      raise Errno::ECONNREFUSED.new("can not connect to instance")
-    end if service_instance[0] == "foo"
-  end
-end
-
-
 describe ZK::HttpClientPooled do
+  include MockHelpers
 
-  context "always using the first service instance returned from Zookeeper" do
+  context "does not encounter any problems" do
 
-    before :each do
-      service_finder = double(ZK::ServiceFinder)
-      service_finder.stub(:watch).with("online-status") { self }
-      service_finder.stub(:instances) { ["foo:4567", "bar:7890"]}
+    subject { create_http_pool_with_first_in_list_strategy("online-status") }
 
-      http_mock = double(Net::HTTP)
-      http_mock.stub(:start) {}
-      http_mock.stub(:request) do |req| 
-        resp = double(Net::HTTPResponse)
-        resp.stub(:http_version) {}
-        resp.stub(:body) {"request url was #{req.path}"}
-        resp
-      end
-
-      strategy = double(ZK::FirstInListStrategy)
-      strategy.stub(:svcname) {"online-status"}
-      strategy.stub(:create_http_connection) { http_mock }
-
-      @http = ZK::HttpClientPooled.new(:connection_strategy => strategy)
-    end
-
-
-    it "makes a request to the instance and pools the connection" do
-      @http.connections_count.should eql(0)
-      resp = @http.request_get "/people/12345"
+    it "makes a request to the first service instance and pools the connection" do
+      subject.connections_count.should eql(0)
+      resp = subject.request_get "/people/12345"
 
       resp.should_not eql(nil)
       resp.body.should eql("request url was /people/12345")
-      @http.connections_count.should eql(1)
+      subject.connections_count.should eql(1)
     end
 
     it "will never use a second connection since requests are synchronous and we are single-threaded" do
       (1..10).each do
-        resp = @http.request_get "/people/4567"
+        resp = subject.request_get "/people/4567"
         resp.body.should eql("request url was /people/4567")
-        @http.connections_count.should eql(1)
+        subject.connections_count.should eql(1)
       end
     end
   end
 
-  context "the connection to the service instance fails" do
-    before  do
+  context "can not establish the connection to the service instance" do
 
-      # strategy = double(ZK::FirstInListStrategy)
-      # strategy.stub(:svcname) {"online-status"}
-      # strategy.stub(:create_http_connection) { mock_connection }
+    subject { create_http_pool_which_raises_error_on_start("online-status") }
 
-      @http = ZK::HttpClientPooled.new(:connection_strategy => ZK::FirstInListStrategyMock.new("online-status"))
+    it "should raise an exception" do
+      subject.connections_count.should eql(0)
+      expect { subject.request_get("/does/not/matter") }.to raise_exception
+      subject.connections_count.should eql(0)
+    end
+  end
+
+  context "gets a TCP exception in a HTTP request" do
+
+    subject { create_http_pool_which_raises_exception_on_request("online-status", Errno::ECONNRESET ) }
+
+    it "should try to use another service instance" do
+      subject.connections_count.should eql(0)
+      subject.request_get("/does/not/matter")
+      subject.connections_count.should eql(1)
+    end
+  end
+
+  context "gets a 5xx HTTP error code in a HTTP request" do
+
+    subject { create_http_pool_which_raises_exception_on_request("online-status", Net::HTTPBadResponse ) }
+
+    it "should try to use another service instance" do
+      subject.connections_count.should eql(0)
+      subject.request_get("/does/not/matter")
+      subject.connections_count.should eql(1)
     end
 
-    it "should use another service instance" do
-      @http.request_get "/people/12345"
-    end
+  end
 
+  context "gets persistent errors when making a request" do
+
+    subject { create_http_pool_which_raises_exception_on_request("online-status", Net::HTTPBadResponse, 2 ) }
+
+    it "should raise an exception" do
+      expect { subject.request_get("/does/not/matter") }.to raise_error PersistentHTTP::Error
+    end
   end
 
 end
